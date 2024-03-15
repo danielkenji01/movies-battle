cad642059d7961f66e96c6f9b3cefcbafdb3c414
danielkenji01 <daniel_kenji_1@hotmail.com>
2024-03-15 21:42:31 +0000

    First commit

---
+++ src/main/java/com/moviesbattle/MoviesBattleApplication.java
@@ -0,0 +1,13 @@
+package com.moviesbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviesbattle/MoviesBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

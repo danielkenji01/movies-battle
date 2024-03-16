b3ba731e9b0fe75c8186488687d5193157842b25
danielkenji01 <daniel_kenji_1@hotmail.com>
2024-03-16 18:15:01 +0000

    initial jpa and h2 structure

--- src/main/java/com/moviesbattle/MoviesBattleApplication.java
+++ src/main/java/com/moviesbattle/MoviesBattleApplication.java
@@ -2,8 +2,10 @@ package com.moviesbattle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.moviesbattle.repository")
 public class MoviesBattleApplication {
 
 	public static void main(String[] args) {

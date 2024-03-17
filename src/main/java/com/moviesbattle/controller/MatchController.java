862d928490307b3bf49de75a6eff354023c41c9f
danielkenji01 <daniel_kenji_1@hotmail.com>
2024-03-17 18:43:47 +0000

    integrating endpoints to get next round and sending an answer

--- src/main/java/com/moviesbattle/controller/MatchController.java
+++ src/main/java/com/moviesbattle/controller/MatchController.java
@@ -1,9 +1,12 @@
 package com.moviesbattle.controller;
 
+import com.moviesbattle.dto.RoundAnswerDto;
+import com.moviesbattle.dto.RoundDto;
 import com.moviesbattle.service.MatchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,16 @@ public class MatchController {
         matchService.startMatch();
 
         return ResponseEntity.ok("New match started successfully");
+    }
+
+    @PostMapping("/next-round")
+    public ResponseEntity<RoundDto> nextRound() throws Exception {
+        return ResponseEntity.ok(matchService.nextRound());
+    }
+
+    @PostMapping("/answer")
+    public ResponseEntity<String> answer(@RequestBody final RoundAnswerDto answerDto) throws Exception {
+        return ResponseEntity.ok(matchService.answer(answerDto));
     }
 
 }

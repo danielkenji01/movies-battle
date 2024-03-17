3b83d1b9b0bf1260edc83a6675ce97864c80e151
danielkenji01 <daniel_kenji_1@hotmail.com>
2024-03-17 17:12:01 +0000

    enable feign client

--- src/main/java/com/moviesbattle/MoviesBattleApplication.java
+++ src/main/java/com/moviesbattle/MoviesBattleApplication.java
@@ -2,10 +2,12 @@ package com.moviesbattle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.moviesbattle.repository")
+//@EnableJpaRepositories(basePackages = "com.moviesbattle.repository")
+@EnableFeignClients
 public class MoviesBattleApplication {
 
 	public static void main(String[] args) {
